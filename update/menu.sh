f25f5f06ac433011292dbd616c2075ee5c9548b8
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-06-01 23:56:18 +0700

    Update menu.sh

--- update/menu.sh
+++ update/menu.sh
@@ -1,126 +1,172 @@
 #!/bin/bash
 clear
-GREEN='\033[0;32m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-NC='\033[0m'
-yl='\e[32;1m'
-bl='\e[36;1m'
-gl='\e[32;1m'
-rd='\e[31;1m'
-mg='\e[0;95m'
-blu='\e[34m'
-op='\e[35m'
-or='\033[1;33m'
-bd='\e[1m'
-color1='\e[031;1m'
-color2='\e[34;1m'
-color3='\e[0m'
-# COLOR VALIDATION
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Kulanbagong1/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
 RED='\033[0;31m'
-NC='\033[0m'
+NC='\e[0m'
+gray="\e[1;30m"
+Blue="\033[1;36m"
 GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# =========================================
+grenbo="\033[1;95m"
+YELL='\033[1;33m'
+BGX="\033[42m"
+UPDATE="https://raw.githubusercontent.com/Kulanbagong1/scvps/main/"
+BOT="https://raw.githubusercontent.com/rizkihdyt6/Panel/main/"
+Repo1="https://raw.githubusercontent.com/Kulanbagong1/izinn/main/"
+UDPCORE="https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2"
+ISP=$(cat /etc/xray/isp)
+NS=$(cat /etc/xray/dns)
+CITY=$(cat /etc/xray/city)
+IPVPS=$(curl -s ipv4.icanhazip.com)
+domain=$(cat /etc/xray/domain)
+RAM=$(free -m | awk 'NR==2 {print $2}')
+USAGERAM=$(free -m | awk 'NR==2 {print $3}')
+MEMOFREE=$(printf '%-1s' "$(free -m | awk 'NR==2{printf "%.2f%", $3*100/$2 }')")
+LOADCPU=$(printf '%-0.00001s' "$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')")
+MODEL=$(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')
+CORE=$(printf '%-1s' "$(grep -c cpu[0-9] /proc/stat)")
+Namee="JengkolOnline | Abdullah | Premium Script"
+DATEVPS=$(date +'%d/%m/%Y')
+TIMEZONE=$(printf '%(%H:%M:%S)T')
+SERONLINE=$(uptime -p | cut -d " " -f 2-10000)
+
+
+ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 3)}' | head -1)"
+bmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $8" "substr ($9, 1 ,3)}' | head -1)"
+
+# // Exporting IP Address
+export MYIP=$( curl -s https://ipinfo.io/ip/ )
+Name=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $2}')
+Exp=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $3}')
+
+clear
+echo -e "\033[96m┌────────────────────────────────────────────────────────────┐${NC}"
+echo -e "\e[1;97m               PROJECT AUTOSCRIPT VPN      \e[0m"
+echo -e "\033[96m────────────────────────────────────────────────────────────${NC}"
+echo -e "\e[1;97m                     INFORMATION                \e[0m"
+echo -e "\033[96m────────────────────────────────────────────────────────────${NC}"
 echo -e ""
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m          ⇱ Menu Utama @XDecrytorId ⇲          \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "❇️ Script By : DecrytorId"
-echo -e "❇️ Exp Script  : Lifetime"
-echo -e "❇️ Version     : Beta"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "❇️ 1) SSH & OpenVPN MENU                                          "
-echo -e "❇️ 2) L2TP MENU                                                   "
-echo -e "❇️ 3) PPTP MENU                                                "
-echo -e "❇️ 4) SSTP MENU                                                "
-echo -e "❇️ 5) WIREGUARD MENU                                            "
-echo -e "❇️ 6) SHADOWSOCKS MENU                                          "
-echo -e "❇️ 7) SHADOWSOCKSR MENU                                          "
-echo -e "❇️ 8) VMESS MENU                                                 "
-echo -e "❇️ 9) VLESS MENU                                               "
-echo -e "❇️ 10) TROJAN GFW MENU                                         "
-echo -e "❇️ 11) TROJAN GO MENU                                            "
-echo -e "❇️ 12) CEK SERVICE VPS                                            "
-echo -e "❇️ 13) PENGATURAN                                                "
-echo -e "❇️ 14) CEK V2RAY "
-echo -e "❇️ 15) EDIT PATCH V2RAY "
-echo -e "❇️ 16) EXIT                                                        "
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m            ⇱ Script By @XDecrytorId ⇲             \E[0m"
-echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-read -p "Select From Options [ 1 - 13 ] : " menu
+echo -e "   ${YELL}System OS${NC}     :   ${putih}$MODEL${NC}"
+echo -e "   ${YELL}Server RAM${NC}    :   ${putih}$RAM MB $NC"
+echo -e "   ${YELL}Uptime Server${NC} :   ${putih}$SERONLINE${NC}"
+echo -e "   ${YELL}Core System${NC}   :   ${putih}$CORE${NC}"
+echo -e "   ${YELL}Date${NC}          :   ${putih}$DATEVPS${NC}"
+echo -e "   ${YELL}Time${NC}          :   ${putih}$TIMEZONE${NC}"
+echo -e "   ${YELL}Domain${NC}        :   ${putih}$domain${NC}"
+echo -e "   ${YELL}NS Domain${NC}     :   ${putih}$NS${NC}"
+echo -e ""
+echo -e "\033[96m└────────────────────────────────────────────────────────────┘${NC}"
+echo -e "\033[96m┌────────────────────────────────────────────────────────────┐${NC}"
+echo -e "\e[1;97m                   CREATE ACCOUNT              \e[0m"
+echo -e "\033[96m────────────────────────────────────────────────────────────${NC}"
+echo -e ""
+echo -e "   ${grenbo}[1]❇️${NC} ${YELL}MENU SSH&OVPN${NC}       | ${grenbo}[7]❇️${NC} ${YELL}MENU SOWDOWSOKS-R${NC}"
+echo -e "   ${grenbo}[2]❇️${NC} ${YELL}MENU VMESS${NC}          | ${grenbo}[8]❇️${NC} ${YELL}MENU L2TP${NC}"
+echo -e "   ${grenbo}[3]❇️${NC} ${YELL}MENU VLESS${NC}          | ${grenbo}[9]❇️${NC} ${YELL}MENU PPTP${NC}"
+echo -e "   ${grenbo}[4]❇️${NC} ${YELL}MENU TROJAN${NC}         | ${grenbo}[10]❇️${NC} ${YELL}MENU SSTP${NC}"
+echo -e "   ${grenbo}[5]❇️${NC} ${YELL}MENU TROJAN GO${NC}      | ${grenbo}[11]❇️${NC} ${YELL}MENU WIREGUARD${NC}"
+echo -e "   ${grenbo}[6]❇️${NC} ${YELL}MENU SOWDOWSOKS${NC}     | ${grenbo}[12]❇️${NC} ${YELL}INSTALL UDP${NC}"
+echo -e ""
+echo -e "\033[96m────────────────────────────────────────────────────────────${NC}"
+echo -e "\e[1;97m                    MENU SETTING              \e[0m"
+echo -e "\033[96m────────────────────────────────────────────────────────────${NC}"
+echo -e "   ${grenbo}[13]❇️${NC} ${YELL}CEK SERVICE VPS${NC}    | ${grenbo}[16]❇️${NC} ${YELL}EDIT PROT V2RAY${NC}"
+echo -e "   ${grenbo}[14]❇️${NC} ${YELL}PENGGATURAN${NC}        | ${grenbo}[17]❇️${NC} ${YELL}ADD NOTIF BOT${NC}"
+echo -e "   ${grenbo}[15]❇️${NC} ${YELL}CEK V2RAY${NC}          | ${grenbo}[18]❇️${NC} ${YELL}ADD BOT PANEL${NC}"
+echo -e "\033[96m────────────────────────────────────────────────────────────${NC}"
+echo -e "\e[1;97m                    NEWBIE TUNNEL              \e[0m"
+echo -e "\033[96m────────────────────────────────────────────────────────────${NC}"
+echo -e ""
+echo -e "   ${YELL}IP VPS${NC}        :  ${putih}$IPVPS${NC}"
+echo -e "   ${YELL}Client Name${NC}   :  ${putih}$Name${NC}"
+echo -e "   ${YELL}Exp Script${NC}    :  ${putih}$Exp${NC}"
+echo -e "   ${YELL}Name Author${NC}   :  ${putih}$Namee${NC}"
+echo -e ""
+echo -e "\033[96m└────────────────────────────────────────────────────────────┘${NC}"
+echo -e ""
+read -p "   Select From Options [ 1 - 8 or x exit ] : " menu
 case $menu in
-1)
-clear
-sshovpnmenu
-;;
-2)
-clear
-l2tpmenu
-;;
-3)
-clear
-pptpmenu
-;;
-4)
-clear
-sstpmenu
-;;
-5)
-clear
-wgmenu
-;;
-6)
-clear
-ssmenu
-;;
-7)
-clear
-ssrmenu
-;;
-8)
-clear
-vmessmenu
-;;
-9)
-clear
-vlessmenu
-;;
-10)
-clear
-trmenu
-;;
-11)
-clear
-trgomenu
-;;
-12)
-clear
-running
-;;
-13)
-clear
-setmenu
-;;
-14)
-clear
-cekxray
-;;
-15)
-clear
-nano /etc/xray/config.json
-;;
-16)
-clear
-exit
-;;
-*)
-clear
-menu
-;;
+1) clear ;
+    sshovpnmenu
+    ;;
+2) clear ;
+    vmessmenu
+    ;;
+3) clear ;
+    vlessmenu
+    ;;
+4) clear ;
+    trmenu
+    ;;
+5) clear ;
+    trgomenu
+    ;;
+6) clear ;
+    ssmenu
+    ;;
+7) clear ;
+    ssrmenu
+    ;;
+8) clear ;
+    l2tpmenu
+    ;;
+9) clear ;
+    pptpmenu
+    ;;      
+10) clear ;
+    sstpmenu
+    ;; 
+11) clear ;
+    wgmenu
+    ;;
+12) clear ;
+    wget --load-cookies /tmp/cookies.txt ${UDPCORE} -O install-udp && rm -rf /tmp/cookies.txt && chmod +x install-udp && ./install-udp
+   ;;        
+13) clear ;
+    running
+    ;;
+14) clear ;
+    setmenu
+    ;;
+15) clear ;
+    cekxray
+    ;;
+16) clear ;
+    nano /etc/xray/config.json
+    ;;   
+17) clear ;
+   bot
+   ;;
+18) clear ;
+  wget ${BOT}xolpanel.sh && chmod +x xolpanel.sh && ./xolpanel.sh
+   ;;
+x) clear ;
+   exit
+   ;;
+*) clear ;
+    menu
+    ;;
 esac
