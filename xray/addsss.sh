86989046c7255d501f616da7d86bee69788ccf43
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-06-02 04:18:51 +0700

    Create addsss.sh

---
+++ xray/addsss.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# My Telegram : https://t.me/anuybazoelk
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- icanhazip.com);
+IZIN=$( curl https://raw.githubusercontent.com/Kulanbagong1/izinn/main/ip | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${CYAN}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${PURPLE}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+echo "Checking VPS"
+clear
+source /var/lib/bzstorevpn/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP
+fi
