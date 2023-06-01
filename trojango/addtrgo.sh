f35cc589a90a21df54facc42a2ed4ea30897d9d7
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-06-02 02:20:45 +0700

    Update addtrgo.sh

--- trojango/addtrgo.sh
+++ trojango/addtrgo.sh
@@ -36,9 +36,9 @@ domain=$(cat /etc/xray/domain)
 else
 domain=$IP
 fi
-trgo="$(cat ~/log-install.txt | grep -w "Tr Go" | cut -d: -f2|sed 's/ //g')"
+trgo="$(cat ~/log-install.txt | grep -w "Trojan Go" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
-		read -rp "Password : " -e user
+		read -rp "User : " -e user
 		user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
 
 		if [[ ${user_EXISTS} == '1' ]]; then
@@ -47,26 +47,34 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 			exit 1
 		fi
 	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
 read -p "Expired (Days) : " masaaktif
 sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
 systemctl restart trojan-go.service
-link="trojan-go://${user}@${domain}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
+link="trojan-go://${uuid}@isi_bug_disini:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=%2Ftrojango#$user"
+link1="trojan://${uuid}@isi_bug_disini:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=%2Ftrojango#$user"
 clear
-echo -e ""
-echo -e "=======-TROJAN-GO-======="
-echo -e "Remarks    : ${user}"
-echo -e "IP/Host    : ${MYIP}"
-echo -e "Address    : ${domain}"
-echo -e "Port       : ${trgo}"
-echo -e "Key        : ${user}"
-echo -e "Encryption : none"
-echo -e "Path       : /trojango"
-echo -e "Created    : $hariini"
-echo -e "Expired    : $exp"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[0;41;36m          TROJAN GO          \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Remarks    : ${user}" | tee -a /etc/log-create-user.log
+echo -e "IP/Host    : ${MYIP}" | tee -a /etc/log-create-user.log
+echo -e "Address    : ${domain}" | tee -a /etc/log-create-user.log
+echo -e "Port       : ${trgo}" | tee -a /etc/log-create-user.log
+echo -e "Key        : ${uuid}" | tee -a /etc/log-create-user.log
+echo -e "Encryption : none" | tee -a /etc/log-create-user.log
+echo -e "Path       : /trojango" | tee -a /etc/log-create-user.log
+echo -e "Created    : $hariini" | tee -a /etc/log-create-user.log
+echo -e "Expired    : $exp" | tee -a /etc/log-create-user.log
 echo -e "========================="
-echo -e "Link TrGo  : ${link}"
+echo -e "Link TrGo  		: ${link}" | tee -a /etc/log-create-user.log
+echo -e "Link TrGo (v2rayNG)	: ${link1}" | tee -a /etc/log-create-user.log
 echo -e "========================="
-echo -e "Script By  @XDecrytorId"
+echo -e "Script Mod By ADAM SIJA"
+echo "" | tee -a /etc/log-create-user.log
+read -n 1 -s -r -p "Press any key to back on menu"
+
+menu
