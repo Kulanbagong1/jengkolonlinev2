320b697a67df8eeb378a16e4853975089f7dd182
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-06-02 14:24:28 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -258,7 +258,7 @@ echo "   - OHP OpenVPN             : 8787"  | tee -a log-install.txt
 echo "   - Trojan Go               : 443"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "    Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta GMT +7"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
