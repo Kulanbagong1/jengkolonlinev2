08929bb2a38f0f821e9d831a73264979aed2f160
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-06-02 15:55:57 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -254,7 +254,7 @@ echo "━━━━━━━━━━━━━━━━━━━━━━━━�
 echo ""  | tee -a log-install.txt
 echo "   - Server Information  Other Features"  | tee -a log-install.txt
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | tee -a log-install.txt
-echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta GMT +7"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
