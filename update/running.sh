c5de8c1562674b863609b0e53ac063cfde9d052c
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-06-03 20:59:17 +0700

    Update running.sh

--- update/running.sh
+++ update/running.sh
@@ -1,201 +1,68 @@
-export red="\e[1;31m"
-export green="\e[0;32m"
-export NC="\e[0m"
-
-# // GIT USER
-export GitUser="Kulanbagong1"
-export MYIP=$(wget -qO- icanhazip.com);
-#source /var/lib/premium-script/ipvps.conf
-# // VPS INFO
+#!/bin/bash
 clear
-Checkstart1=$(ip route | grep default | cut -d ' ' -f 3 | head -n 1);
-if [[ $Checkstart1 == "venet0" ]]; then 
-    clear
-    lan_net="venet0"
-    typevps="OpenVZ"
-    sleep 1
-else
-    clear
-    lan_net="eth0"
-    typevps="KVM"
-    sleep 1
-fi
-
-# // VPS ISP INFORMATION
-echo -e "\e[32mloading...\e[0m"
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Kulanbagong1/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
 clear
-#Domain
-#IP=$(wget -qO- icanhazip.com);
-#source /var/lib/premium-script/ipvps.conf
-#if [[ "$IP" = "" ]]; then
-#domain=$(cat /usr/local/etc/xray/domain)"
-#else
-#domain=$IP
-#fi
-export ITAM='\033[0;30m'
-echo -e "$ITAM"
-export NAMAISP=$( curl -s ipinfo.io/org | cut -d " " -f 2-10  )
-export REGION=$( curl -s ipinfo.io/region )
-export COUNTRY=$( curl -s ipinfo.io/country )
-export WAKTU=$( curl -s ipinfo.ip/timezone )
-export CITY=$( curl -s ipinfo.io/city )
-export REGION=$( curl -s ipinfo.io/region )
-export WAKTUE=$( curl -s ipinfo.io/timezone )
-export koordinat=$( curl -s ipinfo.io/loc )
+RED='\033[0;31m'
+NC='\e[0m'
+gray="\e[1;30m"
+Blue="\033[1;36m"
+GREEN='\033[0;32m'
+grenbo="\033[1;95m"
+YELL='\033[1;33m'
+BGX="\033[42m"
+#UPDATE="https://raw.githubusercontent.com/Kulanbagong1/scvps/main/"
+#BOT="https://raw.githubusercontent.com/rizkihdyt6/Panel/main/"
+Repo1="https://raw.githubusercontent.com/Kulanbagong1/izinn/main/"
+#UDPCORE="https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S3IE25v_fyUfCLslnujFBSBMNunDHDk2"
+#ISP=$(cat /etc/xray/isp)
+NS=$(cat /etc/xray/dns)
+#CITY=$(cat /etc/xray/city)
+IPVPS=$(curl -s ipv4.icanhazip.com)
+domain=$(cat /etc/xray/domain)
+RAM=$(free -m | awk 'NR==2 {print $2}')
+#USAGERAM=$(free -m | awk 'NR==2 {print $3}')
+#MEMOFREE=$(printf '%-1s' "$(free -m | awk 'NR==2{printf "%.2f%", $3*100/$2 }')")
+#LOADCPU=$(printf '%-0.00001s' "$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')")
+MODEL=$(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')
+CORE=$(printf '%-1s' "$(grep -c cpu[0-9] /proc/stat)")
+Namee="JengkolOnline | Abdullah | Premium Script"
+DATEVPS=$(date +'%d/%m/%Y')
+TIMEZONE=$(printf '%(%H:%M:%S)T')
+SERONLINE=$(uptime -p | cut -d " " -f 2-10000)
 
-# // TOTAL RAM
-export tram=$( free -m | awk 'NR==2 {print $2}' )
-export uram=$( free -m | awk 'NR==2 {print $3}' )
-export fram=$( free -m | awk 'NR==2 {print $4}' )
-export swap=$( free -m | awk 'NR==4 {print $2}' )
 
-# // USERNAME
-echo -e "$NC"
-rm -f /usr/bin/user
-export username=$( curl -sS https://raw.githubusercontent.com/Kulanbagong1/izinn/main/ip | grep $MYIP | awk '{print $2}' )
-echo "$username" > /usr/bin/user
+#ttoday="$(vnstat | grep today | awk '{print $8" "substr ($9, 1, 3)}' | head -1)"
+#bmon="$(vnstat -m | grep `date +%G-%m` | awk '{print $8" "substr ($9, 1 ,3)}' | head -1)"
 
-# // ORDER ID
-#rm -f /usr/bin/ver
-#export user=$( curl -sS https://raw.githubusercontent.com/${GitUser}/izinn/main/ipvps.conf | grep $MYIP | awk '{print $3}' )
-#echo "$user" > /usr/bin/ver
+# // Exporting IP Address
+export MYIP=$( curl -s https://ipinfo.io/ip/ )
+Name=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $2}')
+Exp=$(curl -sS ${Repo1}ip | grep $MYIP | awk '{print $3}')
 
-# // VALIDITY
-rm -f /usr/bin/e
-export valid=$( curl -sS https://raw.githubusercontent.com/Kulanbagong1/izinn/main/ip | grep $MYIP | awk '{print $4}' )
-echo "$valid" > /usr/bin/e
-
-# // DETAIL ORDER
-export username=$(cat /usr/bin/user)
-#export oid=$(cat /usr/bin/ver)
-export exp=$(cat /usr/bin/e)
-
-# // TYPE PROCS
-export totalcore="$(grep -c "^processor" /proc/cpuinfo)" 
-export totalcore+=" Core"
-export corediilik="$(grep -c "^processor" /proc/cpuinfo)" 
-export tipeprosesor="$(awk -F ': | @' '/model name|Processor|^cpu model|chip type|^cpu type/ {
-                        printf $2;
-                        exit
-                        }' /proc/cpuinfo)"
-
-# // SHELL VER
-export shellversion=""
-export shellversion=Bash
-export shellversion+=" Version" 
-export shellversion+=" ${BASH_VERSION/-*}" 
-export versibash=$shellversion
-
-# // OS INFO
-source /etc/os-release
-export Versi_OS=$VERSION
-export ver=$VERSION_ID
-export Tipe=$NAME
-export URL_SUPPORT=$HOME_URL
-export basedong=$ID
-
-# // DOWNLOAD
-export download=`grep -e "lo:" -e "wlan0:" -e "eth0" /proc/net/dev  | awk '{print $2}' | paste -sd+ - | bc`
-export downloadsize=$(($download/1073741824))
-
-# // UPLOAD
-export upload=`grep -e "lo:" -e "wlan0:" -e "eth0" /proc/net/dev | awk '{print $10}' | paste -sd+ - | bc`
-export uploadsize=$(($upload/1073741824))
-
-# // CPU INFO
-export cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
-export cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
-export cpu_usage+=" %"
-
-# // OS UPTIME
-export uptime="$(uptime -p | cut -d " " -f 2-10)"
-
-# // KERNEL
-export kernelku=$(uname -r)
-
-# // DATE
-export harini=`date -d "0 days" +"%d-%m-%Y"`
-export jam=`date -d "0 days" +"%X"`
-
-# // DNS PATH
-export tipeos2=$(uname -m)
-
-#Domain
-#IP=$(wget -qO- icanhazip.com);
-#source /var/lib/premium-script/ipvps.conf
-#if [[ "$IP" = "" ]]; then
-domain=$(cat /etc/xray/domain)"
-#else"
-#domain=$IP
-#fi
-
-echo -e "\e[32mloading...\e[0m"
 clear
-echo -e ""
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "Your VPS Information :"
-echo -e "\e[0;32mSCRIPT VPS\e[0m"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "Operating System Information :"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "VPS Type    : $typevps"
-echo -e "OS Arch     : $tipeos2"
-echo -e "Hostname    : $HOSTNAME"
-echo -e "OS Name     : $Tipe"
-echo -e "OS Version  : $Versi_OS"
-echo -e "OS URL      : $URL_SUPPORT"
-echo -e "OS BASE     : $basedong"
-echo -e "OS TYPE     : Linux / Unix"
-echo -e "Bash Ver    : $versibash"
-echo -e "Kernel Ver  : $kernelku"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "Hardware Information :"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "Processor   : $tipeprosesor"
-echo -e "Proc Core   : $totalcore"
-echo -e "Virtual     : $typevps"
-echo -e "Cpu Usage   : $cpu_usage"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "System Status / System Information :"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "Uptime      : $uptime"
-echo -e "Total RAM   : $tram MB"
-echo -e "Used RAM    : $uram MB"
-echo -e "Avaible RAM : $fram MB"
-echo -e "Download    : $downloadsize GB ( From Startup/VPS Booting )"
-echo -e "Upload      : $uploadsize GB ( From Startup/VPS Booting )"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "Internet Service Provider Information :"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-
-echo -e "Public IP   : $MYIP"
-echo -e "Domain      : $Domen"
-echo -e "ISP Name    : $NAMAISP"
-echo -e "Region      : $REGION "
-echo -e "Country     : $COUNTRY"
-echo -e "City        : $CITY "
-echo -e "Time Zone   : $WAKTUE"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "Time & Date & Location & Coordinate Information :"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "Location    : $COUNTRY"
-echo -e "Coordinate  : $koordinat"
-echo -e "Time Zone   : $WAKTUE"
-echo -e "Date        : $harini"
-echo -e "Time        : $jam ( WIB )"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "\e[1;32mSTATUS SCRIPT :\e[0m"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e "\e[0;34mUser        :\e[0m $username"
-#echo -e "\e[0;34mOrder ID    :\e[0m $oid"
-echo -e "\e[0;34mExpired     :\e[0m $exp"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo -e ""
-echo -e "              \e[0;32m[\e[1;36mSYSTEM STATUS INFORMATION\e[0;32m]\e[0m"
-echo -e "             \e[0;34m=============================\e[0m"
-echo -e ""
-echo -e "\e[1;33mSTATUS SSH & OPEN VPN:\e[0m"
-echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 
 # CHEK STATUS 
 l2tp_status=$(systemctl status xl2tpd | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
@@ -475,7 +342,7 @@ totalram=$(($total_ram/1024))
 #uptime="$(uptime -p | cut -d " " -f 2-10)"
 
 # KERNEL TERBARU
-kernelku=$(uname -r)
+#kernelku=$(uname -r)
 
 # WAKTU SEKARANG 
 #harini=`date -d "0 days" +"%d-%m-%Y"`
@@ -487,24 +354,24 @@ kernelku=$(uname -r)
 # GETTING DOMAIN NAME
 #Domen="$(cat /etc/xray/domain)"
 echo -e ""
-#echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-#echo -e "\E[44;1;39m              ⇱ Sytem Information ⇲             \E[0m"
-#echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-#echo -e "❇️ Hostname    : $HOSTNAME"
-#echo -e "❇️ OS Name     : $Tipe"
-#echo -e "Processor   : $tipeprosesor"
-#echo -e "Proc Core   :$totalcore"
-#echo -e "Virtual     :$typevps"
-#echo -e "Cpu Usage   :$cpu_usage"
-#echo -e "❇️ Total RAM   : ${totalram}MB"
-#echo -e "❇️ Public IP   : $MYIP"
-#echo -e "❇️ Domain      : $Domen"
-#echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-#echo -e "\E[44;1;39m          ⇱ Subscription Information ⇲          \E[0m"
-#echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-#echo -e "❇️ Client Name : $Name"
-#echo -e "❇️ Exp Script  : $Exp"
-#echo -e "❇️ Version     : Latest Version"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m              ⇱ Sytem Information ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "❇️ Hostname    : $HOSTNAME"
+echo -e "❇️ OS Name     : $Tipe"
+echo -e "Processor   : $tipeprosesor"
+echo -e "Proc Core   :$totalcore"
+echo -e "Virtual     :$typevps"
+echo -e "Cpu Usage   :$cpu_usage"
+echo -e "❇️ Total RAM   : ${totalram}MB"
+echo -e "❇️ Public IP   : $MYIP"
+echo -e "❇️ Domain      : $Domen"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m          ⇱ Subscription Information ⇲          \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "❇️ Client Name : $Name"
+echo -e "❇️ Exp Script  : $Exp"
+echo -e "❇️ Version     : Latest Version"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "\E[44;1;39m            ⇱ Service Information ⇲             \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
